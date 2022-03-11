df4f23afa53e0cb201967aae532e6088576adf58
sc2bat <87482415+sc2bat@users.noreply.github.com>
2022-03-11 13:05:12 +0900

    Add files via upload
    코테시 문제유형을 파악하고 그에 해당하는 해답을 제시해준다
    Map, HashMap
    
    Map 은 인터페이스 HashMap 은클래스

---
+++ hash_test02/phone_book_01.java
@@ -0,0 +1,30 @@
+package t220311_01;
+
+import java.util.Arrays;
+
+public class phone_book_01 {
+	public static void main(String[] args) {
+//		String[] phone_book = {"119", "97674223", "1195524421"};
+		String[] phone_book = {"123","456","789"};
+//		String[] phone_book = {"12","123","1235","567","88"};
+		Solution a = new Solution();
+		System.out.println(a.solution(phone_book));
+		
+	}
+	
+	static class Solution {
+	    public boolean solution(String[] phone_book) {
+	        boolean answer = true;
+	        Arrays.sort(phone_book);
+	        for(int i = 0; i < phone_book.length; i++) {
+	        	System.out.println(phone_book[i]);
+	        }
+	        for(int i = 0; i < phone_book.length; i++){
+	            if(phone_book[i+1].startsWith(phone_book[i])){ 
+	                return false;
+	            }
+	        }
+	        return answer;
+	    }
+	}
+}
---
+++ hash_test02/phone_book_02.java
@@ -0,0 +1,34 @@
+package t220311_01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class phone_book_02 {
+	public static void main(String[] args) {
+//		String[] phone_book = {"119", "97674223", "1195524421"};
+		String[] phone_book = {"123","456","789"};
+//		String[] phone_book = {"12","123","1235","567","88"};
+		Solution a = new Solution();
+		System.out.println(a.solution(phone_book));	
+		
+	}
+	
+	static class Solution {
+	    public boolean solution(String[] phone_book) {
+	        boolean answer = true;
+	        Map<String, Object> hm = new HashMap<String, Object>();
+	        for(int i = 0; i < phone_book.length; i++) {
+	        	hm.put(phone_book[i], i);
+//	        	System.out.println(phone_book[i]);
+	        }
+	        for(int i = 0; i < phone_book.length; i++) {
+	        	for(int j = 0; j < phone_book[i].length(); j++) {
+	        		if(hm.containsKey(phone_book[i].substring(0, j))) {
+	        			return false;
+	        		}
+	        	}
+	        }
+	        return answer;
+	    }
+	}
+}
